1dfe9d86aabdd9740c14a1556cc6d23c4072eaa1
hgollnick <henrique.gollnick@gmail.com>
2025-07-02 19:20:46 -0300

    Update debugpy initialization to improve clarity; remove wait for client message

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,6 @@
 import debugpy
-
 debugpy.listen(("0.0.0.0", 5678))
-print("Waiting for debugger attach...")
-debugpy.wait_for_client()
+print("Debugpy is listening on 0.0.0.0:5678")
 
 import logging
 from fastapi import FastAPI, Body, Request, HTTPException
