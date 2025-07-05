eff0783aaca3c212e8d276b1077549edaa79ad67
hgollnick <henrique.gollnick@gmail.com>
2025-07-05 15:44:07 -0300

    Remove CodeAgent, FastAgent, and GeneralAgent classes to streamline agent structure

--- backend/src/commons/default/code_agent.py
+++
@@ -1,5 +0,0 @@
-from ..ollama_service import OllamaAgent
-
-class CodeAgent(OllamaAgent):
-    def __init__(self, model, ollama_url):
-        super().__init__(model, ollama_url)
--- backend/src/commons/default/fast_agent.py
+++
@@ -1,5 +0,0 @@
-from ..ollama_service import OllamaAgent
-
-class FastAgent(OllamaAgent):
-    def __init__(self, model, ollama_url):
-        super().__init__(model, ollama_url)
